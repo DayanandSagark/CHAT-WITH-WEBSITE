27d1e81d3e20f68781230736a1182889232e5d5e
DayanandSagark <dayanand_87@yahoo.co.in>
2024-02-25 18:36:38 -0600

    Update retriver chain changes

--- src/app.py
+++ src/app.py
@@ -6,8 +6,21 @@ from bs4 import BeautifulSoup
 from langchain_core.messages import AIMessage,HumanMessage
 # Added lang chain community document loaders - webbased loader
 from langchain_community.document_loaders import WebBaseLoader
-# Add recursive char text splitter
+# Add recursive char text splitter - document chunks
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+# importing lang chain vector database - chroma
+from langchain_community.vectorstores import Chroma
+
+# importing embaddings open ai 
+from langchain_openai import OpenAIEmbeddings,ChatOpenAI
+from dotenv import load_dotenv
+# prompt template
+from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
+# complete retriever chain
+from langchain.chains import create_history_aware_retriever,create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+load_dotenv()
 
 
 
@@ -18,10 +31,38 @@ def get_vectorstore_from_url(url):
     #get the text in document form 
     loader = WebBaseLoader(url)
     document = loader.load()
+
+    # Split document into chunks
     text_splitter = RecursiveCharacterTextSplitter()
     document_chunks = text_splitter.split_documents(document)
-    return document_chunks
 
+    vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
+    return vector_store
+
+def get_context_retrieval_chain(vector_store):
+    llm = ChatOpenAI()
+    retriever = vector_store.as_retriever()
+
+    prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user","{input}"),
+        ("user","Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+        ])
+
+    retriever_chain = create_history_aware_retriever(llm,retriever,prompt)
+    return retriever_chain
+
+
+def get_coverstaional_rag_chain(retriver_chain):
+    llm =ChatOpenAI()
+    prompt = ChatPromptTemplate.from_messages([
+        ("system","Answer the user questions based on the below context:\n\n{context}"),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user","{input}"),
+
+    ])
+    stuff_documents_chain = create_stuff_documents_chain(llm,prompt)
+    create_retrieval_chain(retriver_chain,stuff_documents_chain)
 # APP CONFIG
 st.set_page_config(page_title="Chat with websites", page_icon = "hello")
 st.title("Chat with websites")
@@ -42,9 +83,10 @@ if website_url is None or website_url == "":
 
 else:
     #pass the web url for the document form
-    document_chunks = get_vectorstore_from_url(website_url)
-    with st.sidebar:
-        st.write(document_chunks)
+    vector_store = get_vectorstore_from_url(website_url)
+    retriever_chain = get_context_retrieval_chain(vector_store)
+    #with st.sidebar:
+     #   st.write(document_chunks)
     # Create input prompts for your website
     # User input
     usr_qry = st.chat_input("Enter your message here ...")
@@ -52,6 +94,12 @@ else:
         response = get_response(usr_qry)
         st.session_state.chat_history.append(HumanMessage(content=usr_qry))
         st.session_state.chat_history.append(AIMessage(content=response))
+
+        retrieved_documents = retriever_chain.invoke({
+            "chat+history" : st.session_state.chat_history,
+             "input" : usr_qry
+        })
+        st.write(retrieved_documents)
         
     #Conversation
     for message in st.session_state.chat_history:
